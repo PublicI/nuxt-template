22d8cf23ef8a12e5b1344f4a4c61e2e1225c1ded
Chris Zubak-Skees <chriszs@gmail.com>
2015-06-14 01:10:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+This is a generator for single-page apps and embeddable graphics for use by the Center for Public Integrity's news dev team, or anyone else who wants in. It sets things up real good so you don't have to worry about it none.
+
+## Getting Started
+
+First, install yo if you don't have yo:
+
+```bash
+npm install -g yo
+```
+
+Then, to install this generator, run:
+
+```bash
+npm install -g generator-publici-app
+```
+
+Finally, initiate the generator:
+
+```bash
+yo publici-app
+```
+
+## License
+
+MIT
---
+++ generators/app/index.js
@@ -0,0 +1,42 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.generators.Base.extend({
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the ' + chalk.red('Public Integrity App') + ' generator!'
+    ));
+
+    var prompts = [{
+      type: 'confirm',
+      name: 'someOption',
+      message: 'Would you like to enable this option?',
+      default: true
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      // To access props later use this.props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+      this.fs.copy(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json')
+      );
+    }
+  },
+
+  install: function () {
+    this.installDependencies();
+  }
+});
