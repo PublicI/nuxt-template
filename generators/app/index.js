7edc5d66c6107e6a8495a8470bad89972e391ca3
Chris Zubak-Skees <chriszs@gmail.com>
2017-03-13 18:38:32 -0400

    Try to fix ES6 template issue another way

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,8 +7,6 @@ module.exports = yeoman.generators.Base.extend({
   constructor: function () {
     yeoman.generators.Base.apply(this, arguments);
 
-    this._.templateSettings.interpolate = /<%=([\s\S]+?)%>/g;
-
     this.s = s;
   },
   prompting: function () {
@@ -44,72 +42,88 @@ module.exports = yeoman.generators.Base.extend({
       this.fs.copyTpl(
         this.templatePath('_package.json'),
         this.destinationPath('package.json'),
-        this
+        this,
+        { interpolate: /<%=([\s\S]+?)%>/g }
       );
     },
     gitignore: function () {
       this.fs.copy(
         this.templatePath('_.gitignore'),
-        this.destinationPath('.gitignore')
+        this.destinationPath('.gitignore'),
+        this,
+        { interpolate: /<%=([\s\S]+?)%>/g }
       );
     },
     config: function () {
       this.fs.copy(
         this.templatePath('config.yml.example'),
-        this.destinationPath('config.yml.example')
+        this.destinationPath('config.yml.example'),
+        this,
+        { interpolate: /<%=([\s\S]+?)%>/g }
       );
     },
     gulpfile: function () {
       this.fs.copyTpl(
         this.templatePath('gulpfile.js'),
         this.destinationPath('gulpfile.js'),
-        this
+        this,
+        { interpolate: /<%=([\s\S]+?)%>/g }
       );
     },
     readme: function () {
       this.fs.copyTpl(
         this.templatePath('README.md'),
         this.destinationPath('README.md'),
-        this
+        this,
+        { interpolate: /<%=([\s\S]+?)%>/g }
       );
     },
     src: function () {
       this.fs.copyTpl(
         this.templatePath('src/*'),
         this.destinationPath('src'),
-        this
+        this,
+        { interpolate: /<%=([\s\S]+?)%>/g }
       );
     },
     data: function () {
       this.fs.copy(
         this.templatePath('src/data/*'),
-        this.destinationPath('src/data')
+        this.destinationPath('src/data'),
+        this,
+        { interpolate: /<%=([\s\S]+?)%>/g }
       );
     },
     script: function () {
       this.fs.copyTpl(
         this.templatePath('src/script/*'),
         this.destinationPath('src/script'),
-        this
+        this,
+        { interpolate: /<%=([\s\S]+?)%>/g }
       );
     },
     lib: function () {
       this.fs.copy(
         this.templatePath('src/script/lib/**'),
-        this.destinationPath('src/script/lib')
+        this.destinationPath('src/script/lib'),
+        this,
+        { interpolate: /<%=([\s\S]+?)%>/g }
       );
     },
     style: function () {
       this.fs.copyTpl(
         this.templatePath('src/style/*'),
         this.destinationPath('src/style'),
-        this
+        this,
+        { interpolate: /<%=([\s\S]+?)%>/g }
       );
     },
     util: function () {
       this.fs.copy(
         this.templatePath('src/util/*'),
-        this.destinationPath('src/util')
+        this.destinationPath('src/util'),
+        this,
+        { interpolate: /<%=([\s\S]+?)%>/g }
       );
     }
   },
