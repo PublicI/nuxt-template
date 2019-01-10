f10232b5b47f557d0f677bb190a613cf36c32d9f
Chris Zubak-Skees <chriszs@gmail.com>
2019-01-10 10:13:57 -0500

    Delete examples, closes #39

--- template/pages/index.vue
+++ template/pages/index.vue
@@ -1,40 +1,18 @@
 <template>
-    <section>
-        <h1>
-            EXAMPLES
-        </h1>
-        <ul class="examples">
-            <li v-for="(example, index) in examples" :key="index" class="example">
-                <nuxt-link :to="{ name: 'id', params: { id: index }}">
-                    {{ example.name }}
-                </nuxt-link>
-            </li>
-        </ul>
-    </section>
+    <div class="basic-text">
+        <!-- content goes here -->
+        It worked!
+    </div>
 </template>
 
 <script>
-import examples from '~/assets/examples.json';
-
 export default {
     data () {
-        return examples;
-    },
-    head () {
-        return {
-            title: 'examples'
-        };
+        return {}; // data goes here
     }
 };
 </script>
 
 <style scoped>
-.examples {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-}
-.example {
-    margin: 10px 0;
-}
+/* CSS goes here */
 </style>
