38a7627b6d9b889f170e031bcd585c11bcfa3952
Chris Zubak-Skees <chriszs@gmail.com>
2019-02-10 11:27:50 -0500

    Try to make sure .gitignore gets copied

--- saofile.js
+++ saofile.js
@@ -33,6 +33,12 @@ module.exports = {
       filters: {
           '**/.DS_Store': false
       }
+    },
+    {
+      type: 'move',
+      patterns: {
+        gitignore: '.gitignore'
+      }
     }
   ],
   async completed() {
