89eff8102d699d2f67b16fbc900d53014a718540
Chris Zubak-Skees <chriszs@gmail.com>
2020-10-10 12:36:03 -0400

    Revise approach to run SAO locally after template

--- README.md
+++ README.md
@@ -1,21 +1,24 @@
-# <%= name %>
+## Requirements
 
-> <%= description %>
+-   [Node.js](https://nodejs.org/)
 
-## Build setup
+## Setup
 
-``` bash
-# serve with hot reload at localhost:3000/<%= name %>
-$ npm run dev
+To start a new project with this template:
 
-# build for production and launch server
-$ npm start
+1. Click [Use this template](https://github.com/PublicI/app-generator/generate).
+2. Specify the repository's name and click "Create repository from template."
+3. Wait for GitHub to generate a new repository.
+4. Clone or open the repository by choosing on of the options under the "↓ Code" button.
+5. Open a terminal and navigate to the new repository's directory. Once in the project's directory, run `npx sao .`.
+6. Answer a few questions and commit the result.
 
-# or generate static page
-$ npm run generate
+If you don't want to start with a GitHub repo, you could run `npx sao github:PublicI/app-generator <name-of-project>` in place of steps 1-6.
 
-# and push to s3
-$ npm run push
+## Development
+
+```sh
+npm run dev
 ```
 
 For detailed explanation on how things work, check out the [Nuxt.js docs](https://github.com/nuxt/nuxt.js).
--- saofile.js
+++ saofile.js
@@ -1,41 +1,54 @@
 module.exports = {
-  prompts() {
-    return [
-      {
-        name: 'name',
-        message: 'Project name',
-        default: this.outFolder,
-        filter: val => val.toLowerCase().replace(/ /g, '-')
-      },
-      {
-        name: 'description',
-        message: 'Project description',
-        default: ``
-      },
-      {
-        name: 'author',
-        message: 'Author',
-        default: 'Center for Public Integrity',
-        store: true
-      },
-      {
-        name: 'host',
-        message: 'Domain',
-        default: 'apps.publicintegrity.org',
-        store: true
-      }
-    ];
-  },
-  actions: [
-    {
-      type: 'add',
-      files: '**',
-      filters: {
-          '**/.DS_Store': false
-      }
-    }
-  ],
-  async completed() {
-    this.gitInit();
-  }
+    prompts() {
+        return [
+            {
+                name: "name",
+                message: "Project name",
+                default: this.outFolder,
+                filter: (val) => val.toLowerCase().replace(/ /g, "-"),
+            },
+            {
+                name: "description",
+                message: "Project description",
+                default: ``,
+            },
+            {
+                name: "author",
+                message: "Author",
+                default: this.gitUser.name,
+                store: true,
+            },
+            {
+                name: "host",
+                message: "Domain",
+                default: "apps.publicintegrity.org",
+                store: true,
+            },
+        ];
+    },
+    actions: [
+        {
+            type: "add",
+            files: "**",
+            filters: {
+                "**/.DS_Store": false,
+            },
+        },
+        {
+            type: "move",
+            patterns: {
+                "README.md.template": "README.md",
+            },
+        },
+    ],
+    async completed() {
+        this.gitInit();
+        await this.npmInstall({
+            npmClient: "npm",
+        });
+        this.showProjectTips();
+
+        this
+            .logger(`To start a development server which serves the project at http://localhost:3000/${this.outDir}, cd to that directory and type npm run dev.`);
+    },
 };
